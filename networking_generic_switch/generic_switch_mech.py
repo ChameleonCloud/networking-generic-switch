128a03a74f757d412f8c20b027cf6974f0b971eb
Paul Ruth <paul.ruth@gmail.com>
2022-04-12 14:45:33 -0400

    push to renci dev

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -533,7 +533,7 @@ class GenericSwitchDriver(api.MechanismDriver):
                           ', port2_name: ' + str(port2_name) +
                           ', port2_vlan: ' + str(port2_vlan)
                           )
-                self.patchpanel.add_patch(patch_id=reservation_id,
+                self.patchpanel_switch.add_patch(patch_id=reservation_id,
                                  port1_name=port1_name,
                                  port1_vlan=port1_vlan,
                                  port2_name=port2_name,
