bfa848b26f0581d14d0f6b96f31e87b2c39558dc
Paul Ruth <paul.ruth@gmail.com>
2022-04-15 17:55:56 -0400

    push to renci dev

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -539,7 +539,8 @@ class GenericSwitchDriver(api.MechanismDriver):
 
                 #LOG.debug("Candidate shadow_port: " + str(shadow_port))
                 #if not 'bindings' in shadow_port['bindings']:
-                if hasattr(shadow_port,'bindings') and shadow_port['bindings'] == None:
+                #if hasattr(shadow_port_candidate,'bindings') and shadow_port_candidate['bindings'] == None:
+                if shadow_port_candidate['bindings'] == None:
                     LOG.debug("port does not have bindings, skipping")
                     continue
                 LOG.debug("project_id " + str(project_id))
