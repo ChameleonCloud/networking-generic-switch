6c5da75147a9d17110141a8c800c7c847a8df39d
Paul Ruth <paul.ruth@gmail.com>
2022-04-16 08:39:51 -0400

    push to renci dev

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -543,9 +543,9 @@ class GenericSwitchDriver(api.MechanismDriver):
                     #LOG.debug("Candidate shadow_port: " + str(shadow_port))
                     #if not 'bindings' in shadow_port['bindings']:
                     #if hasattr(shadow_port_candidate,'bindings') and shadow_port_candidate['bindings'] == None:
-                    if shadow_port_candidate['bindings'] == None and 'project_id' in shadow_port_candidate['bindings'].keys() and  'reservation_id' in shadow_port_candidate['bindings'].keys() :
-                        LOG.debug("port does not have bindings or stitchport info, skipping")
-                        continue
+                    #if shadow_port_candidate['bindings'] == None and 'project_id' in shadow_port_candidate['bindings'].keys() and  'reservation_id' in shadow_port_candidate['bindings'].keys() :
+                    #    LOG.debug("port does not have bindings or stitchport info, skipping")
+                    #    continue
                     LOG.debug("\nproject_id " + str(project_id))
                     LOG.debug("\nreservation_id " + str(reservation_id))
                     LOG.debug("\nshadow_port_candidate['bindings']['project_id'] " + str(shadow_port_candidate['bindings']['project_id']))
