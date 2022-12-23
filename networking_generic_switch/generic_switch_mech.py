6436017fdca8eeff3b755bfd735b029bb263e37e
Paul Ruth <pruth@renci.org>
2022-12-23 11:44:11 -0500

    update

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -781,7 +781,7 @@ class GenericSwitchDriver(api.MechanismDriver):
         #                                                                                       "type": "shadow"}, status = 'ACTIVE', vif_details = None, vif_type = 'unbound', vnic_type = 'normal')]
         LOG.debug("bindings: " + str(shadow_port.bindings))
         LOG.debug("profile: " + str(shadow_port.bindings[0]['profile']))
-        for k,v in shadow_port.bindings[0]['profile']:
+        for k,v in shadow_port.bindings[0]['profile'].items():
             LOG.debug(""+str(key)+", "+str(val))
 
 
