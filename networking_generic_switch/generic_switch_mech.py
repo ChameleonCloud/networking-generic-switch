09b5df9e5abe9be89f4a09c637fa9b825e4ce9f4
Paul Ruth <pruth@renci.org>
2023-01-02 13:02:22 -0500

    update

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -941,7 +941,7 @@ class GenericSwitchDriver(api.MechanismDriver):
                 # Update user port binding profile
                 #user_port_binding = port['binding:profile']
                 new_user_port_binding_profile = {}
-                for k, v in new_user_port_binding_profile.items():
+                for k, v in port['binding:profile'].items():
                     new_user_port_binding_profile[k] = v
 
                 #new_user_port_binding_profile['stitchport'] = new_shadow_binding_profile['stitchport']
